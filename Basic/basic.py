ca0bb1790b7367b285d0c3dcb9525682fcf58f80
jakemraz <jakemraz100@gmail.com>
2020-03-09 23:18:42 +0900

    pandas, mlbasic, basic

--- Basic/basic.py
+++ Basic/basic.py
@@ -6,3 +6,8 @@ scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf
 # {5: 35044.51299744237, 25: 29016.41319191076, 50: 27405.930473214907, 100: 27282.50803885739, 250: 27893.822225701646, 500: 29454.18598068598}
 best_tree_size = min(scores, key=scores.get)
 
+# one line if
+value = 10 if x > 0 else 20
+
+# Array Copy
+train_X_plus = train_X.copy()
--- Basic/pandas.py
+++ Basic/pandas.py
@@ -10,3 +10,85 @@ y = home_data.SalePrice
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
 X.describe()
+
+# Basic command
+df.shape
+df.info()
+df.columns
+df.dtypes
+df.head()
+df.tail()
+
+df.isnull().sum() # count the number of null for each columns
+df.isnull().sum() / df.shape[0] # portion of null for each columns
+df['column_name'].unique() # check unique values
+df['A'] = df['B'].str[:5]  # make a new column
+df['A'] = df['B'].str[-5:]
+
+df = df.drop(['column'], axis=1)
+df = df.drop(['row'])
+
+df= df.rename(columns={'A' : 'B'}) # change column name
+df= pd.DataFrame(df, columns=['A', 'C', 'B']) # change column order
+result = pd.concat([df1, df2], ignore_index=True) # concat two columns
+df.duplicated() # check duplicate
+df.drop_duplicates() # drop duplicate
+
+
+# Series and DataFrame
+a = X_train['LotFrontage'] # can not put multiple item
+type(a)
+# >> pandas.core.series.Series
+
+a = X_train[['LotFrontage']]
+type(a)
+# >> pandas.core.frame.DataFrame
+
+
+## pandas series
+series_var.isnull()
+# >> return Series with values replaced to boolean
+series_var.any()
+# >> return True:numpy.bool_ if there is True at least
+
+
+
+## Save to CSV from Pandas Data Frame
+# Save predictions in format used for competition scoring
+output = pd.DataFrame({'Id': X_test.index,
+                       'SalePrice': preds_test})
+output.to_csv('submission.csv', index=False)
+
+
+##########################
+##### Missing Values #####
+##########################
+
+## Drop
+# Get names of columns with missing values
+cols_with_missing = [col for col in X_train.columns
+                     if X_train[col].isnull().any()] 
+# Drop columns in training and validation data
+reduced_X_train = X_train.drop(cols_with_missing, axis=1) # axis=0:row/1:colume
+
+## Imputation
+from sklearn.impute import SimpleImputer
+my_imputer = SimpleImputer()
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+# what is different between fit_transform and transform?
+
+# Imputation removed column names; put them back
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+## An Extention to Imputation
+# Make copy to avoid changing original data (when imputing)
+X_train_plus = X_train.copy()
+X_valid_plus = X_valid.copy()
+
+# Make new columns indicating what will be imputed
+for col in cols_with_missing:
+    X_train_plus[col + '_was_missing'] = X_train_plus[col].isnull()
+    X_valid_plus[col + '_was_missing'] = X_valid_plus[col].isnull()
+
