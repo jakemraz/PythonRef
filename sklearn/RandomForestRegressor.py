fe931d2e5ead92e8785b66ff12bface5fd944d6e
jakemraz <jakemraz100@gmail.com>
2020-03-21 17:57:29 +0900

    sklearn: add scroer using mae

--- sklearn/RandomForestRegressor.py
+++ sklearn/RandomForestRegressor.py
@@ -6,3 +6,11 @@ forest_model = RandomForestRegressor(random_state=1, n_estimators=10)
 forest_model.fit(train_X, train_y)
 melb_preds = forest_model.predict(val_X)
 print(mean_absolute_error(val_y, melb_preds))
+
+
+# function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
