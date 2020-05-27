b3c7d9275fceb349ffe5df85b0b1d68a5b7beb52
jakemraz <jakemraz100@gmail.com>
2020-05-27 11:11:03 +0900

    make decimal_decoder return float

--- aws/decimal_encoder.py
+++ aws/decimal_encoder.py
@@ -1,7 +1,10 @@
 import decimal
 import json
 class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, decimal.Decimal):
-            return int(obj)
-        return super(DecimalEncoder, self).default(obj)
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
